1d3ed38a49d3e0d4e34b8277a8df703eec6a3333
snsrrhc3078 <snsrrhc3078@gmail.com>
2023-02-17 17:55:41 +0900

    [feat] 20230217 강의 프로젝트
    스프링 서버에 REST 통신을 통해 안드로이드 환경에서 게시판 구현하기 실습

--- SpringBoard/src/main/java/com/edu/springboard/client/controller/RestNoticeController.java
+++ SpringBoard/src/main/java/com/edu/springboard/client/controller/RestNoticeController.java
@@ -1,10 +1,14 @@
 package com.edu.springboard.client.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.springboard.domain.Notice;
@@ -12,20 +16,42 @@ import com.edu.springboard.exception.NoticeException;
 import com.edu.springboard.model.notice.NoticeService;
 
 @RestController
+@RequestMapping("/rest")
 public class RestNoticeController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private NoticeService noticeService;
 	
-	@PostMapping("/rest/notice/regist")
+	@PostMapping("/notice/regist")
 	public String regist(Notice notice) {
 		
 		logger.info(notice.getTitle());
 		logger.info(notice.getWriter());
 		logger.info(notice.getContent());
-		//noticeService.insert(notice);
+		noticeService.insert(notice);
 		return "test";
+	}
+	
+	//글 목록 요청 처리
+	@GetMapping("/notice/list")
+	public List<Notice> getList() {
+		
+		//3단계
+		List list = noticeService.selectAll();
+		
+		//4단계 : jsp 뷰로 가져갈 일이 없으므로, 4단계 생략
+		
+		return list;
+	}
+	
+	//상세페이지 요청 처리
+	@GetMapping("/notice/detail")
+	public Notice getDetail(int notice_idx) {
+		//3단계
+		Notice notice = noticeService.select(notice_idx);
+		
+		return notice;
 	}
 	
 	@ExceptionHandler(NoticeException.class)
